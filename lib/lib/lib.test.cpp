5c0a59309cf70545e6f66016a31b315856d046d6
XzoRit <xzorit@hotmail.com>
2021-11-17 13:55:42 +0100

    use all kinds of load/save/serialize functions

--- lib/lib/lib.test.cpp
+++ lib/lib/lib.test.cpp
@@ -1,28 +1,25 @@
+#include <boost/utility/identity_type.hpp>
 #include <cereal/archives/json.hpp>
 #include <cereal/cereal.hpp>
 #include <cereal/types/utility.hpp>
 #include <cereal/types/vector.hpp>
 
+#include <boost/mpl/list.hpp>
 #include <boost/test/unit_test.hpp>
+#include <boost/utility/identity_type.hpp>
 
+#include <libs/test/include/boost/test/tools/old/interface.hpp>
+#include <libs/test/include/boost/test/unit_test_log.hpp>
 #include <ostream>
 #include <sstream>
 #include <tuple>
 #include <utility>
 #include <vector>
 
-namespace
+namespace v1
 {
 struct simple_data
 {
-    simple_data() = default;
-    simple_data(int aa, int bb, float cc)
-        : a{aa}
-        , b{bb}
-        , c{cc}
-    {
-    }
-
     template <class Archive>
     void serialize(Archive& ar)
     {
@@ -42,127 +39,122 @@ std::ostream& operator<<(std::ostream& str, const simple_data& a)
 
     return str;
 }
+} // namespace v1
 
-struct complex_data
+namespace v2
 {
-    using data_type = std::vector<std::pair<int, simple_data>>;
+struct simple_data
+{
+    bool operator==(const simple_data&) const = default;
 
-    complex_data() = default;
-    complex_data(data_type d)
-        : data{std::move(d)}
-    {
-    }
-
-    template <class Archive>
-    void save(Archive& ar) const
-    {
-        ar(cereal::make_nvp("data", data));
-    }
-
-    template <class Archive>
-    void load(Archive& ar)
-    {
-        ar(data);
-    }
-
-    bool operator==(const complex_data&) const = default;
-
-    data_type data{};
+    int a{};
+    int b{};
+    float c{};
 };
 
-std::ostream& operator<<(std::ostream& str, const complex_data& a)
+template <class Archive>
+void serialize(Archive& ar, simple_data& a)
 {
-    for (const auto& [i, d] : a.data)
-    {
-        str << '(' << i << ',' << d << ')';
-    }
+    ar(cereal::make_nvp("a", a.a), cereal::make_nvp("b", a.b), cereal::make_nvp("c", a.c));
+}
+
+std::ostream& operator<<(std::ostream& str, const simple_data& a)
+{
+    str << "a=" << a.a << " b=" << a.b << " c=" << a.c;
 
     return str;
 }
+} // namespace v2
 
-BOOST_AUTO_TEST_SUITE(lib_tests)
-
-BOOST_AUTO_TEST_CASE(load_save_simple_data)
+namespace v3
 {
-    std::stringstream ss{};
-    const simple_data a{1, 22, 33.0};
+struct simple_data
+{
+    template <class Archive>
+    void save(Archive& archive) const
     {
-        cereal::JSONOutputArchive archive{ss};
-
-        archive(cereal::make_nvp("simple_data", a));
+        archive(a, b, c);
     }
 
-    BOOST_REQUIRE(!ss.str().empty());
-    BOOST_CHECK_EQUAL(ss.str(), R"({
-    "simple_data": {
-        "a": 1,
-        "b": 22,
-        "c": 33.0
-    }
-})");
-
+    template <class Archive>
+    void load(Archive& archive)
     {
-        cereal::JSONInputArchive iarchive(ss);
-
-        simple_data b{};
-        iarchive(b);
-
-        BOOST_CHECK_EQUAL(a, b);
+        archive(a, b, c);
     }
+
+    bool operator==(const simple_data&) const = default;
+
+    int a{};
+    int b{};
+    float c{};
+};
+
+std::ostream& operator<<(std::ostream& str, const simple_data& a)
+{
+    str << "a=" << a.a << " b=" << a.b << " c=" << a.c;
+
+    return str;
+}
+} // namespace v3
+
+namespace v4
+{
+struct simple_data
+{
+    bool operator==(const simple_data&) const = default;
+
+    int a{};
+    int b{};
+    float c{};
+};
+
+template <class Archive>
+void save(Archive& archive, const simple_data& a)
+{
+    archive(a.a, a.b, a.c);
 }
 
-BOOST_AUTO_TEST_CASE(load_save_complex_data)
+template <class Archive>
+void load(Archive& archive, simple_data& a)
+{
+    archive(a.a, a.b, a.c);
+}
+
+std::ostream& operator<<(std::ostream& str, const simple_data& a)
+{
+    str << "a=" << a.a << " b=" << a.b << " c=" << a.c;
+
+    return str;
+}
+} // namespace v4
+
+BOOST_AUTO_TEST_SUITE(cereal_tests)
+
+using test_types = boost::mpl::list<v1::simple_data, v2::simple_data, v3::simple_data, v4::simple_data>;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(serialize_free_func, A, test_types)
 {
     std::stringstream ss{};
-    const complex_data a{{{0, {0, 0, 0.}}, {1, {1, 1, 1.}}, {2, {2, 2, 2.}}}};
-
+    const A a{};
     {
         cereal::JSONOutputArchive archive{ss};
 
-        archive(cereal::make_nvp("complex_data", a));
+        archive(cereal::make_nvp("serialize_free_func", a));
     }
 
     BOOST_REQUIRE(!ss.str().empty());
-    BOOST_CHECK_EQUAL(ss.str(), R"({
-    "complex_data": {
-        "data": [
-            {
-                "first": 0,
-                "second": {
-                    "a": 0,
-                    "b": 0,
-                    "c": 0.0
-                }
-            },
-            {
-                "first": 1,
-                "second": {
-                    "a": 1,
-                    "b": 1,
-                    "c": 1.0
-                }
-            },
-            {
-                "first": 2,
-                "second": {
-                    "a": 2,
-                    "b": 2,
-                    "c": 2.0
-                }
-            }
-        ]
-    }
-})");
+    BOOST_TEST_CHECKPOINT("archive content: " << ss.str());
 
     {
-        cereal::JSONInputArchive iarchive(ss);
+        A b{};
+        {
+            cereal::JSONInputArchive iarchive(ss);
 
-        complex_data b{};
-        iarchive(b);
+            iarchive(b);
+        }
 
         BOOST_CHECK_EQUAL(a, b);
     }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
-} // namespace
