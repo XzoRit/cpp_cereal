4f22d984e4c4e8c12404a94c35e2818453036f9f
XzoRit <xzorit@hotmail.com>
2021-11-18 09:11:54 +0100

    use private load/save/serialize functions

--- lib/lib/lib.test.cpp
+++ lib/lib/lib.test.cpp
@@ -1,3 +1,4 @@
+#include "cereal/access.hpp"
 #include <boost/utility/identity_type.hpp>
 #include <cereal/archives/json.hpp>
 #include <cereal/cereal.hpp>
@@ -154,11 +155,78 @@ std::ostream& operator<<(std::ostream& str, const simple_data& a)
 }
 } // namespace v4
 
+namespace v5
+{
+class simple_data
+{
+  public:
+    bool operator==(const simple_data&) const = default;
+
+  private:
+    friend std::ostream& operator<<(std::ostream& str, const simple_data& a);
+    friend class cereal::access;
+
+    template <class Archive>
+    void serialize(Archive& archive)
+    {
+        archive(a, b, c);
+    }
+
+    int a{random_value<int>(gen)};
+    int b{random_value<int>(gen)};
+    float c{random_value<float>(gen)};
+};
+
+std::ostream& operator<<(std::ostream& str, const simple_data& a)
+{
+    str << "a=" << a.a << " b=" << a.b << " c=" << a.c;
+
+    return str;
+}
+} // namespace v5
+
+namespace v6
+{
+class simple_data
+{
+  public:
+    bool operator==(const simple_data&) const = default;
+
+  private:
+    friend std::ostream& operator<<(std::ostream& str, const simple_data& a);
+    friend class cereal::access;
+
+    template <class Archive>
+    void save(Archive& archive) const
+    {
+        archive(a, b, c);
+    }
+
+    template <class Archive>
+    void load(Archive& archive)
+    {
+        archive(a, b, c);
+    }
+
+    int a{random_value<int>(gen)};
+    int b{random_value<int>(gen)};
+    float c{random_value<float>(gen)};
+};
+
+std::ostream& operator<<(std::ostream& str, const simple_data& a)
+{
+    str << "a=" << a.a << " b=" << a.b << " c=" << a.c;
+
+    return str;
+}
+} // namespace v6
+
 BOOST_AUTO_TEST_SUITE(cereal_tests)
 
-using test_types = boost::mpl::list<v1::simple_data, v2::simple_data, v3::simple_data, v4::simple_data>;
+using test_types = boost::mpl::
+    list<v1::simple_data, v2::simple_data, v3::simple_data, v4::simple_data, v5::simple_data, v6::simple_data>;
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(serialize_free_func, A, test_types)
+BOOST_AUTO_TEST_CASE_TEMPLATE(serialize_types, A, test_types)
 {
     std::stringstream ss{};
     const A a{};
